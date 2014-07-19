7693d1d2031c8e50f60ae011ac26b46d4e04af93
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-07-19 11:15:50 +0200

    Test setting of log level

--- main_test.go
+++ main_test.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"os"
 	"testing"
 
+	"github.com/mlafeldt/chef-runner/log"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestLogLevel(t *testing.T) {
+	tests := map[string]int{
+		"":      log.LevelInfo,
+		"debug": log.LevelDebug,
+		"info":  log.LevelInfo,
+		"warn":  log.LevelWarn,
+		"error": log.LevelError,
+		"DEBUG": log.LevelDebug,
+		"INFO":  log.LevelInfo,
+		"WARN":  log.LevelWarn,
+		"ERROR": log.LevelError,
+		"foo":   log.LevelInfo,
+	}
+	defer os.Setenv("CHEF_RUNNER_LOG", "")
+	for env, level := range tests {
+		os.Setenv("CHEF_RUNNER_LOG", env)
+		assert.Equal(t, level, logLevel())
+	}
+}
 
 func TestBuildRunList(t *testing.T) {
 	tests := []struct {
