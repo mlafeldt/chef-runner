701f6e0716074b87a44336cca9c17bddc7ced5e4
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-07-23 21:26:14 +0200

    Replace `vagrant ssh` with ssh proper
    Based on the output of `vagrant ssh-config`.

--- vagrant/vagrant_test.go
+++ vagrant/vagrant_test.go
@@ -13,14 +13,4 @@ func TestString(t *testing.T) {
 	assert.Equal(t, expect, actual)
 }
 
-func TestSSHCommand_DefaultMachine(t *testing.T) {
-	expect := []string{"vagrant", "ssh", "default", "-c", "uname -a"}
-	actual := vagrant.NewClient("").SSHCommand("uname -a")
-	assert.Equal(t, expect, actual)
-}
-
-func TestSSHCommand_CustomMachine(t *testing.T) {
-	expect := []string{"vagrant", "ssh", "some-machine", "-c", "uname -a"}
-	actual := vagrant.NewClient("some-machine").SSHCommand("uname -a")
-	assert.Equal(t, expect, actual)
-}
+// TODO: test RunCommand machinery
