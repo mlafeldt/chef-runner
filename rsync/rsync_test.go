155d0ce8171c558d2f8f9e8cd8d455786c391b0d
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-07-27 16:32:40 +0200

    Consolidate rsync tests

--- rsync/rsync_test.go
+++ rsync/rsync_test.go
@@ -8,63 +8,78 @@ import (
 )
 
 var commandTests = []struct {
-	client rsync.Client
-	src    []string
-	dst    string
-	cmd    []string
+	client    rsync.Client
+	src       []string
+	dst       string
+	cmd       []string
+	errString string
 }{
 	{
-		rsync.Client{},
-		[]string{"a"}, "b",
-		[]string{"rsync", "a", "b"},
+		client: rsync.Client{},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "a", "b"},
 	},
 	{
-		rsync.Client{},
-		[]string{"a", "b"}, "c",
-		[]string{"rsync", "a", "b", "c"},
+		client: rsync.Client{},
+		src:    []string{"a", "b"},
+		dst:    "c",
+		cmd:    []string{"rsync", "a", "b", "c"},
 	},
 	{
-		rsync.Client{Archive: true},
-		[]string{"a"}, "b",
-		[]string{"rsync", "--archive", "a", "b"},
+		client: rsync.Client{Archive: true},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "--archive", "a", "b"},
 	},
 	{
-		rsync.Client{Delete: true},
-		[]string{"a"}, "b",
-		[]string{"rsync", "--delete", "a", "b"},
+		client: rsync.Client{Delete: true},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "--delete", "a", "b"},
 	},
 	{
-		rsync.Client{Verbose: true},
-		[]string{"a"}, "b",
-		[]string{"rsync", "--verbose", "a", "b"},
+		client: rsync.Client{Verbose: true},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "--verbose", "a", "b"},
 	},
 	{
-		rsync.Client{Exclude: []string{"x", "y"}},
-		[]string{"a"}, "b",
-		[]string{"rsync", "--exclude", "x", "--exclude", "y", "a", "b"},
+		client: rsync.Client{Exclude: []string{"x", "y"}},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "--exclude", "x", "--exclude", "y", "a", "b"},
 	},
 	{
-		rsync.Client{Archive: true, Delete: true, Exclude: []string{"x"}},
-		[]string{"a"}, "b",
-		[]string{"rsync", "--archive", "--delete", "--exclude", "x", "a", "b"},
+		client: rsync.Client{Archive: true, Delete: true, Exclude: []string{"x"}},
+		src:    []string{"a"},
+		dst:    "b",
+		cmd:    []string{"rsync", "--archive", "--delete", "--exclude", "x", "a", "b"},
+	},
+	{
+		client:    rsync.Client{},
+		src:       []string{},
+		dst:       "b",
+		cmd:       nil,
+		errString: "no source given",
+	},
+	{
+		client:    rsync.Client{},
+		src:       []string{"a"},
+		dst:       "",
+		cmd:       nil,
+		errString: "no destination given",
 	},
 }
 
 func TestCommand(t *testing.T) {
 	for _, test := range commandTests {
 		cmd, err := test.client.Command(test.src, test.dst)
-		if assert.NoError(t, err) {
-			assert.Equal(t, test.cmd, cmd)
+		if test.errString == "" {
+			assert.NoError(t, err)
+		} else {
+			assert.EqualError(t, err, test.errString)
 		}
+		assert.Equal(t, test.cmd, cmd)
 	}
-}
-
-func TestCopy_MissingSource(t *testing.T) {
-	err := rsync.DefaultClient.Copy([]string{}, "a/b")
-	assert.EqualError(t, err, "no source given")
-}
-
-func TestCopy_MissingDestination(t *testing.T) {
-	err := rsync.DefaultClient.Copy([]string{"a"}, "")
-	assert.EqualError(t, err, "no destination given")
 }
