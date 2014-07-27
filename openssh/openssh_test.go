862dd4c010dd2dce44a0dcb62d98c6b82819f02c
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-07-27 18:16:58 +0200

    Consolidate openssh tests

--- openssh/openssh_test.go
+++ openssh/openssh_test.go
@@ -40,6 +40,7 @@ var newClientTests = []struct {
 			Port: 1234,
 		},
 	},
+	// Check for errors
 	{
 		host:      "some-host:abc",
 		client:    nil,
@@ -61,89 +62,105 @@ func TestNewClient(t *testing.T) {
 
 func TestString(t *testing.T) {
 	expect := "OpenSSH (host: some-host)"
-	c, err := openssh.NewClient("some-host")
-	assert.NoError(t, err)
+	c, _ := openssh.NewClient("some-host")
 	assert.Equal(t, expect, c.String())
 }
 
 var commandTests = []struct {
-	client openssh.Client
-	cmd    []string
+	client    openssh.Client
+	cmd       string
+	result    []string
+	errString string
 }{
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host: "some-host",
 		},
-		[]string{"ssh", "some-host", "uname -a"},
+		cmd:    "uname -a",
+		result: []string{"ssh", "some-host", "uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host: "some-host",
 			User: "some-user",
 		},
-		[]string{"ssh", "-l", "some-user", "some-host", "uname -a"},
+		cmd:    "uname -a",
+		result: []string{"ssh", "-l", "some-user", "some-host", "uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host: "some-host",
 			Port: 1234,
 		},
-		[]string{"ssh", "-p", "1234", "some-host", "uname -a"},
+		cmd:    "uname -a",
+		result: []string{"ssh", "-p", "1234", "some-host", "uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host:        "some-host",
 			PrivateKeys: []string{"some-key", "another-key"},
 		},
-		[]string{"ssh", "-i", "some-key", "-i", "another-key",
+		cmd: "uname -a",
+		result: []string{"ssh", "-i", "some-key", "-i", "another-key",
 			"some-host", "uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host: "some-host",
 			Options: map[string]string{
 				"SomeOption":    "yes",
 				"AnotherOption": "no",
 			},
 		},
-		[]string{"ssh", "-o", "AnotherOption=no", "-o", "SomeOption=yes",
-			"some-host", "uname -a"},
+		cmd: "uname -a",
+		result: []string{"ssh", "-o", "AnotherOption=no",
+			"-o", "SomeOption=yes", "some-host", "uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host:        "some-host",
 			User:        "some-user",
 			Port:        1234,
 			PrivateKeys: []string{"some-key"},
 			Options:     map[string]string{"SomeOption": "yes"},
 		},
-		[]string{"ssh", "-l", "some-user", "-p", "1234", "-i", "some-key",
-			"-o", "SomeOption=yes", "some-host", "uname -a"},
+		cmd: "uname -a",
+		result: []string{"ssh", "-l", "some-user", "-p", "1234",
+			"-i", "some-key", "-o", "SomeOption=yes", "some-host",
+			"uname -a"},
 	},
 	{
-		openssh.Client{
+		client: openssh.Client{
 			Host:       "some-host",
 			ConfigFile: "some/config/file",
 		},
-		[]string{"ssh", "-F", "some/config/file", "some-host", "uname -a"},
+		cmd: "uname -a",
+		result: []string{"ssh", "-F", "some/config/file", "some-host",
+			"uname -a"},
+	},
+	// Check for errors
+	{
+		client:    openssh.Client{},
+		cmd:       "uname -a",
+		result:    nil,
+		errString: "no host given",
+	},
+	{
+		client:    openssh.Client{Host: "some-host"},
+		cmd:       "",
+		result:    nil,
+		errString: "no command given",
 	},
 }
 
 func TestCommand(t *testing.T) {
 	for _, test := range commandTests {
-		cmd, err := test.client.Command("uname -a")
-		if assert.NoError(t, err) {
-			assert.Equal(t, test.cmd, cmd)
+		result, err := test.client.Command(test.cmd)
+		if test.errString == "" {
+			assert.NoError(t, err)
+		} else {
+			assert.EqualError(t, err, test.errString)
 		}
+		assert.Equal(t, test.result, result)
 	}
-}
-
-func TestRunCommand_MissingCommand(t *testing.T) {
-	err := openssh.Client{Host: "some-host"}.RunCommand("")
-	assert.EqualError(t, err, "no command given")
-}
-
-func TestRunCommand_MissingHost(t *testing.T) {
-	err := openssh.Client{}.RunCommand("uname -a")
-	assert.EqualError(t, err, "no host given")
 }
