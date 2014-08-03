10f32b9d14d3431ca6ee9fa7564dd04c724711e1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-03 19:10:03 +0200

    Remove temporary Librarian-Chef files from destination

--- librarian/librarian.go
+++ librarian/librarian.go
@@ -1,19 +1,39 @@
 package librarian
 
 import (
+	"os"
+	"path"
+	"path/filepath"
+
 	"github.com/mlafeldt/chef-runner/exec"
 	"github.com/mlafeldt/chef-runner/util"
 )
 
-func Command(path string) []string {
+func Command(dst string) []string {
 	var cmd []string
 	if util.FileExist("Gemfile") {
 		cmd = []string{"bundle", "exec"}
 	}
-	cmd = append(cmd, "librarian-chef", "install", "--path", path)
+	cmd = append(cmd, "librarian-chef", "install", "--path", dst)
 	return cmd
 }
 
-func InstallCookbooks(path string) error {
-	return exec.RunCommand(Command(path))
+func removeTempFiles(dst string) error {
+	tmpDirs, err := filepath.Glob(path.Join(dst, "*", "tmp", "librarian"))
+	if err != nil {
+		return err
+	}
+	for _, dir := range tmpDirs {
+		if err := os.RemoveAll(dir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InstallCookbooks(dst string) error {
+	if err := exec.RunCommand(Command(dst)); err != nil {
+		return err
+	}
+	return removeTempFiles(dst)
 }
