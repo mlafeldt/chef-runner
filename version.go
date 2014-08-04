fddc8620351b21bb65729d3287b7882d010f689d
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-04 02:41:09 +0200

    Start development of v0.5.0

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 import "runtime"
 
 // The current version of chef-runner.
-const Version = "v0.4.0"
+const Version = "v0.5.0.dev"
 
 // The Git version that is being compiled. This string contains tag and commit
 // information. It will be filled in by the compiler.
