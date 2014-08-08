d7905aea77b01b3eaee95d4e9f3b471dfbbbc5c0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-08 11:14:36 +0200

    Document util package

--- util/util.go
+++ util/util.go
@@ -1,3 +1,4 @@
+// Package util provides various utility functions.
 package util
 
 import (
@@ -7,11 +8,14 @@ import (
 	"strings"
 )
 
+// FileExist reports whether a file or directory exists.
 func FileExist(name string) bool {
 	_, err := os.Stat(name)
 	return err == nil
 }
 
+// BaseName, as the basename Unix tool, deletes any prefix ending with the last
+// slash character present in a string, and a suffix, if given.
 func BaseName(s, suffix string) string {
 	base := path.Base(s)
 	if suffix != "" {
@@ -20,6 +24,7 @@ func BaseName(s, suffix string) string {
 	return base
 }
 
+// TempDir creates a new temporary directory to be used by chef-runner.
 func TempDir() (string, error) {
 	return ioutil.TempDir("", "chef-runner-")
 }
