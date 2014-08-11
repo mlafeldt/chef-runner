b7dd75506bb2a0742a006d2cbc6a64a7c144d2fb
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-11 16:22:26 +0200

    Document rsync package

--- rsync/rsync.go
+++ rsync/rsync.go
@@ -1,3 +1,4 @@
+// Package rsync provides a wrapper around the fast rsync file copying tool.
 package rsync
 
 import (
@@ -6,19 +7,35 @@ import (
 	"github.com/mlafeldt/chef-runner/exec"
 )
 
+// A Client is an rsync client. It allows you to copy files from one location to
+// another using rsync and supports the tool's most useful command-line options.
 type Client struct {
-	Archive  bool
-	Delete   bool
-	Compress bool
-	Verbose  bool
-	Exclude  []string
+	// Archive, if true, enables archive mode.
+	Archive bool
 
+	// Delete, if true, deletes extraneous files from destination directories.
+	Delete bool
+
+	// Compress, if true, compresses file data during the transfer.
+	Compress bool
+
+	// Verbose, if true, increases rsync's verbosity.
+	Verbose bool
+
+	// Exclude contains files to be excluded from the transfer.
+	Exclude []string
+
+	// RemoteShell specifies the remote shell to use, e.g. ssh.
 	RemoteShell string
-	RemoteHost  string
+
+	// RemoteHost specifies the remote host to copy files to/from.
+	RemoteHost string
 }
 
+// DefaultClient is a usable rsync client without any options enabled.
 var DefaultClient = &Client{}
 
+// Command returns the rsync command that will be executed when Copy is called.
 func (c Client) Command(dst string, src ...string) ([]string, error) {
 	if len(src) == 0 {
 		return nil, errors.New("no source given")
@@ -49,6 +66,7 @@ func (c Client) Command(dst string, src ...string) ([]string, error) {
 		cmd = append(cmd, "--exclude", x)
 	}
 
+	// FIXME: Only copies files to a remote host, not the other way around.
 	if c.RemoteShell != "" {
 		if c.RemoteHost == "" {
 			return nil, errors.New("no remote host given")
@@ -62,6 +80,7 @@ func (c Client) Command(dst string, src ...string) ([]string, error) {
 	return cmd, nil
 }
 
+// Copy uses rsync to copy one or more src files to dst.
 func (c Client) Copy(dst string, src ...string) error {
 	cmd, err := c.Command(dst, src...)
 	if err != nil {
