f2684fe01eef43c5f7832b7aed1d3822e2ee9568
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-19 12:14:36 +0200

    Document all parts of the driver packages

--- driver/ssh/driver.go
+++ driver/ssh/driver.go
@@ -1,5 +1,4 @@
-// Package ssh implements the driver.Driver interface. The implementation is
-// just a simple wrapper around openssh and rsync.
+// Package ssh implements a driver based on OpenSSH.
 package ssh
 
 import (
@@ -10,12 +9,14 @@ import (
 	"github.com/mlafeldt/chef-runner/rsync"
 )
 
+// Driver is a driver based on SSH.
 type Driver struct {
 	host        string
 	sshClient   *openssh.Client
 	rsyncClient *rsync.Client
 }
 
+// NewDriver creates a new SSH driver that communicates with the given host.
 func NewDriver(host string) (*Driver, error) {
 	sshClient, err := openssh.NewClient(host)
 	if err != nil {
@@ -29,14 +30,17 @@ func NewDriver(host string) (*Driver, error) {
 	return &Driver{host, sshClient, rsyncClient}, nil
 }
 
+// RunCommand runs the specified command on the host.
 func (drv Driver) RunCommand(command string) error {
 	return drv.sshClient.RunCommand(command)
 }
 
+// Upload copies files to the host.
 func (drv Driver) Upload(dst string, src ...string) error {
 	return drv.rsyncClient.Copy(dst, src...)
 }
 
+// String returns the driver's name.
 func (drv Driver) String() string {
 	return fmt.Sprintf("SSH driver (host: %s)", drv.sshClient.Host)
 }
