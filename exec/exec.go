e7b0703c4f1981a2ce60827e619363e6b1c4b12c
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-08-19 11:28:25 +0200

    Document exec package

--- exec/exec.go
+++ exec/exec.go
@@ -1,3 +1,5 @@
+// Package exec runs external commands. It's a wrapper around Go's os/exec
+// package that allows to stub command execution for testing.
 package exec
 
 import (
@@ -8,8 +10,12 @@ import (
 	"github.com/mlafeldt/chef-runner/log"
 )
 
+// The RunnerFunc type is an adapter to use any function for running commands.
 type RunnerFunc func(args []string) error
 
+// DefaultRunner is the default function used to run commands. It calls
+// os/exec.Run so that stdout and stderr are written to the terminal.
+// DefaultRunner also logs all executed commands.
 func DefaultRunner(args []string) error {
 	log.Debugf("exec: %s\n", strings.Join(args, " "))
 	cmd := goexec.Command(args[0], args[1:]...)
@@ -20,10 +26,13 @@ func DefaultRunner(args []string) error {
 
 var runnerFunc = DefaultRunner
 
+// SetRunnerFunc registers the function f to run all future commands.
 func SetRunnerFunc(f RunnerFunc) {
 	runnerFunc = f
 }
 
+// RunCommand runs the specified command using the currently registered
+// RunnerFunc.
 func RunCommand(args []string) error {
 	return runnerFunc(args)
 }
