3bf3d4178138aada5a8ff8c33a51f078fd4f3850
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-10-14 21:30:29 +0200

    Add bin stubs for Windows

---
+++ testdata/bin/vagrant.bat
@@ -0,0 +1,14 @@
+:: Vagrant stub for testing
+
+@echo off
+
+echo Host default
+echo  HostName 127.0.0.1
+echo  User vagrant
+echo  Port 2200
+echo  UserKnownHostsFile /dev/null
+echo  StrictHostKeyChecking no
+echo  PasswordAuthentication no
+echo  IdentityFile /Users/mlafeldt/.vagrant.d/insecure_private_key
+echo  IdentitiesOnly yes
+echo  LogLevel FATAL
