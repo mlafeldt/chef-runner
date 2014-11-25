f36cdea84768348a4f92a1c8e51476f8fdb03b33
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-11-25 19:15:47 +0100

    Partial matching of Kitchen instance names
    By parsing `.kitchen.yml` for a list of all available instances and
    searching the list for the provided instance name using partial
    matching.
    
    Partial matching allows the user to specify only a portion of the
    instance name. For example, `chef-runner -K ubuntu` will provision the
    instance "default-ubuntu-1404" if that's the first instance with
    "ubuntu" in its name.
    
    Fixes #23.

--- driver/kitchen/driver.go
+++ driver/kitchen/driver.go
@@ -7,11 +7,11 @@ import (
 	"io/ioutil"
 	"path"
 	"strconv"
+	"strings"
 
 	"github.com/mlafeldt/chef-runner/log"
 	"github.com/mlafeldt/chef-runner/openssh"
 	"github.com/mlafeldt/chef-runner/rsync"
-	"github.com/mlafeldt/chef-runner/util"
 	"gopkg.in/yaml.v2"
 )
 
@@ -31,11 +31,59 @@ var defaultSSHOptions = [...]string{
 	"LogLevel FATAL",
 }
 
+type kitchenConfigEntry struct {
+	Name string `yaml:"name"`
+}
+
+type kitchenConfig struct {
+	Platforms []kitchenConfigEntry `yaml:"platforms"`
+	Suites    []kitchenConfigEntry `yaml:"suites"`
+}
+
 type instanceConfig struct {
 	Hostname string `yaml:"hostname"`
 	Username string `yaml:"username"`
 	SSHKey   string `yaml:"ssh_key"`
 	Port     string `yaml:"port"`
+}
+
+func readInstanceNames() ([]string, error) {
+	data, err := ioutil.ReadFile(".kitchen.yml")
+	if err != nil {
+		return nil, err
+	}
+
+	var config kitchenConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, suite := range config.Suites {
+		for _, platform := range config.Platforms {
+			s := suite.Name + "-" + platform.Name
+			s = strings.Replace(s, "_", "-", -1)
+			s = strings.Replace(s, ".", "", -1)
+			names = append(names, s)
+		}
+	}
+	return names, nil
+}
+
+func findInstanceName(instance string) (string, error) {
+	instanceNames, err := readInstanceNames()
+	if err != nil {
+		return "", err
+	}
+	log.Debugf("Kitchen instances = %s\n", instanceNames)
+
+	for _, name := range instanceNames {
+		// Return first match
+		if strings.Contains(name, instance) {
+			return name, nil
+		}
+	}
+	return "", errors.New("Kitchen instance not found")
 }
 
 func readInstanceConfig(instance string) (*instanceConfig, error) {
@@ -73,8 +121,9 @@ func readInstanceConfig(instance string) (*instanceConfig, error) {
 // Test Kitchen instance. Under the hood the instance's YAML configuration is
 // parsed to get a working SSH configuration.
 func NewDriver(instance string, sshOptions, rsyncOptions []string) (*Driver, error) {
-	if !util.FileExist(".kitchen.yml") {
-		return nil, errors.New("Kitchen YAML file not found")
+	instance, err := findInstanceName(instance)
+	if err != nil {
+		return nil, err
 	}
 
 	config, err := readInstanceConfig(instance)
